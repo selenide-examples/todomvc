d9951873642e6024b7aa851fcdfc69bd9dd89ae5
Johannes Bühler <johannes@gradle.com>
2020-12-03 00:22:26 +0100

    add improvements to dependency resolution conflicts

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,27 +7,39 @@ repositories {
 }
 
 dependencies {
-  testImplementation("com.codeborne:selenide:5.15.1")
-  testImplementation("org.junit.jupiter:junit-jupiter-engine:5.7.0")
-  testRuntimeOnly("org.slf4j:slf4j-simple:1.7.30")
-}
-
-sourceSets {
-  main {
-    java.srcDir("src/main/java")
-  }
-  test {
-    java.srcDir("src/test/java")
-  }
+    components.all<NettyBomAlignmentRule>()
+    testImplementation(platform("org.junit:junit-bom:5.7.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.testng:testng:7.3.0")
+    testImplementation("com.codeborne:selenide:5.15.1")
+    testRuntimeOnly("org.slf4j:slf4j-simple:1.7.30")
+    testRuntimeOnly("com.browserup:browserup-proxy-core:2.1.1")
+    constraints {
+        implementation("com.google.guava:guava:27.1-jre") {
+            because("latest compatible version with all dependencies")
+        }
+    }
 }
 
 java {
-  sourceCompatibility = JavaVersion.VERSION_11
-  targetCompatibility = JavaVersion.VERSION_11
+  toolchain {
+      languageVersion.set(JavaLanguageVersion.of(11))
+  }
 }
 
 tasks.withType<Test>().configureEach {
   useJUnitPlatform()
   testLogging.showExceptions = true
   systemProperties["selenide.headless"] = System.getProperty("selenide.headless")
+}
+
+open class NettyBomAlignmentRule: ComponentMetadataRule {
+    override fun execute(ctx: ComponentMetadataContext) {
+        ctx.details.run {
+            if (id.group.startsWith("io.netty")) {
+                // declare that Netty modules belong to the platform defined by the Netty BOM
+                belongsTo("io.netty:netty-bom:${id.version}", false)
+            }
+        }
+    }
 }
